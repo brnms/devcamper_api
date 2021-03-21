23ef4877ddaff8d50a5dcabd0043570c7fa26b23
Bruno Santos <bruno.matasantos@gmail.com>
2021-03-21 13:04:27 -0300

    Adding Populate, Virtuals and Cascade delete

--- models/Bootcamps.js
+++ models/Bootcamps.js
@@ -98,6 +98,9 @@ const BootcampSchema =  new mongoose.Schema({
         default: Date.now
       }
 
+}, {
+  toJSON: {virtuals: true},
+  toObject: {virtuals: true},
 });
 
 // Create bootcamp slug from the name
@@ -123,6 +126,21 @@ BootcampSchema.pre('save', async function(next) {
   // Do not save adress in DB
   this.adress = undefined;
   next()
+});
+
+// Cascade delete courses when a bootcamp is deleted
+BootcampSchema.pre('remove', async function(next) {
+  console.log(`Courses being removed from bootcamp ${this.id}`)
+  await this.model('Course').deleteMany({ bootcamp: this._id });
+  next();
+})
+
+// Reverse populate with virtuals
+BootcampSchema.virtual('courses', {
+  ref: 'Course',
+  localField: '_id',
+  foreignField: 'bootcamp',
+  justOne: false
 });
 
 module.exports = mongoose.model('Bootcamp', BootcampSchema);
