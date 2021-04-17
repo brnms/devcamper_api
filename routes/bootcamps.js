049495febfea2c72b5dbfc77f9631c07bc33e01b
Bruno Santos <bruno.matasantos@gmail.com>
2021-04-17 14:37:49 -0300

    Adding user CRUD by admin

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -10,13 +10,13 @@ const {
 } = require('../controllers/bootcamps');
 
 const Bootcamp = require('../models/Bootcamps');
-const advancedResults = require('../middleware/advancedResults');
 
 // Include other resource routers
 const courseRouter = require('./courses');
 
 const router = express.Router();
 
+const advancedResults = require('../middleware/advancedResults');
 const { protect, authorize } = require('../middleware/auth');
 
 // Re-route into other resource routers
--- routes/courses.js
+++ routes/courses.js
@@ -2,11 +2,11 @@ const express = require('express');
 const { getCourses, getCourse, addCourse, updateCourse, deleteCourse } = require('../controllers/courses');
 
 const Course = require('../models/Course');
-const advancedResults = require('../middleware/advancedResults')
+
 
 const router = express.Router({ mergeParams: true });
 
-
+const advancedResults = require('../middleware/advancedResults');
 const { protect, authorize } = require('../middleware/auth');
 
 router
