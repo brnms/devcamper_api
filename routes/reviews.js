b1da026512cf0b81778de51f4333d038e4e7e0c5
Bruno Santos <bruno.matasantos@gmail.com>
2021-04-19 17:20:53 -0300

    Adding Update and Delete Reviews functionality

--- routes/reviews.js
+++ routes/reviews.js
@@ -2,7 +2,9 @@ const express = require('express');
 const { 
     getReviews,
     getReview,
-    addReview
+    addReview,
+    updateReview,
+    deleteReview
  } = require('../controllers/reviews');
 
 const Review = require('../models/Review');
@@ -23,7 +25,10 @@ router
 )
     .post(protect, authorize('user', 'admin'), addReview);
 
-router.route('/:id')
+router
+    .route('/:id')
     .get(getReview)
+    .put(protect, authorize('user', 'admin'), updateReview)
+    .delete(protect, authorize('user', 'admin'), deleteReview);
 
 module.exports = router;
