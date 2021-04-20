f991b100c18d135113161ce6892f00a0f159d5aa
Bruno Santos <bruno.matasantos@gmail.com>
2021-04-20 15:11:55 -0300

    Adding security dependencies

--- routes/auth.js
+++ routes/auth.js
@@ -2,11 +2,12 @@ const express = require('express');
 const { 
     register,
     login,
+    logout,
     getMe,
     forgotPassword,
     resetPassword,
     updateDetails,
-    updatePassword
+    updatePassword,
 } = require('../controllers/auth');
 
 const router = express.Router();
@@ -15,6 +16,7 @@ const { protect } = require('../middleware/auth')
 
 router.post('/register', register);
 router.post('/login', login);
+router.get('/logout', logout);
 router.get('/me', protect, getMe);
 router.put('/updatedetails', protect, updateDetails);
 router.put('/updatepassword', protect, updatePassword);
--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const morgan = require('morgan');
 const colors = require('colors');
 const fileupload = require('express-fileupload');
 const cookieParser = require('cookie-parser');
+const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
+const xss = require('xss-clean');
 const errorHandler = require('./middleware/error');
 const connectDB = require('./config/db');
 
@@ -38,6 +41,15 @@ if (process.env.NODE_ENV === 'development') {
 
 // File uploading
 app.use(fileupload());
+
+// Sanitize data
+app.use(mongoSanitize());
+
+// Prevent XSS attacls
+app.use(xss());
+
+// Set security headers
+app.use(helmet());
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
